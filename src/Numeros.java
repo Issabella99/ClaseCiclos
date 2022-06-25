18901861e69f5c705da7f77859a3802482e48023
Issabella99 <rivillasisabella@gmail.com>
2022-06-25 08:44:35 -0500

    Ciclo for numeros del 1 al 100 competo

---
+++ src/Numeros.java
@@ -0,0 +1,9 @@
+public class Numeros {
+
+    public static void main(String[] args) {
+        //Ejemplo escribir numeros del 1 al 100
+        for (int i =1; i <= 100; i ++){
+            System.out.println("Numero: " +i);
+        }
+    }
+}
