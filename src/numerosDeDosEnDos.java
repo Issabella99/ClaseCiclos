c0de34ac248ba26ef62225124943f77d425cc26b
Issabella99 <rivillasisabella@gmail.com>
2022-06-25 08:55:59 -0500

    Metodo terminado

--- src/numerosDeDosEnDos.java
+++ src/numerosDeDosEnDos.java
@@ -1,2 +1,9 @@
 public class numerosDeDosEnDos {
+    public static void main(String[] args) {
+        //Imprimir 10 numeros sumados de 2 en 2, empezando en 2
+        for (int i = 2; i <= 20; i = i + 2) {
+            System.out.println("Numero " + i);
+
+        }
+    }
 }
