74076398f8701a03842d3b6cf940d901df32acdf
Issabella99 <rivillasisabella@gmail.com>
2022-06-25 09:05:30 -0500

    Ciclo 1 3

---
+++ src/Serie1_3.java
@@ -0,0 +1,10 @@
+public class Serie1_3 {
+    public static void main(String[] args) {
+        //Imprimir 1 3 1 3 1 3
+        for (int i = 0; i <= 2; i++){
+            System.out.println("1");
+            System.out.println("3");
+        }
+
+    }
+}
