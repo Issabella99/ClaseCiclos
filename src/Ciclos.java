28284fe42b20df1dd223a988371dfdd76b950ac8
Issabella99 <rivillasisabella@gmail.com>
2022-06-25 08:27:38 -0500

    Clase numeros

--- src/Ciclos.java
+++ src/Ciclos.java
@@ -1,4 +1,19 @@
+import javax.swing.*;
+
 public class Ciclos {
+
+    public Ciclos() {
+
+    }
+
+    public void ciclofor() {
+        for (int i = 0; i < 10; i++) {
+            JOptionPane.showMessageDialog(null, "Hola mundo");
+        }
+
+
+    }
+
     public static void main(String[] args) {
 
     }
